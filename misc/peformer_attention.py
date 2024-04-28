4202ba78e133ff35230a549445544514760a35e6
aapupu <94039288+aapupu@users.noreply.github.com>
2024-04-28 15:59:45 +0800

    Create peformer_attention.py

---
+++ misc/peformer_attention.py
@@ -0,0 +1,3 @@
+# Modified based on
+# https://github.com/google-research/google-research/blob/master/performer/fast_attention/jax/fast_attention.py &
+# https://github.com/TencentAILabHealthcare/scBERT/tree/master/performer_pytorch
