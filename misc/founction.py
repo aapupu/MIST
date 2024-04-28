072f51bdc784487e91a80da734602c313abe47a1
aapupu <94039288+aapupu@users.noreply.github.com>
2024-04-28 16:24:58 +0800

    Create founction.py

---
+++ misc/founction.py
@@ -0,0 +1,10 @@
+"""
+
+"""
+
+from sklearn.neighbors import KNeighborsClassifier
+import pandas as pd
+import numpy as np
+
+from sklearn.metrics.pairwise import cosine_similarity, euclidean_distances
+from sklearn.preprocessing import MinMaxScaler
