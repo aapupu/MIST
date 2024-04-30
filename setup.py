43cd3cefb1269e991cdc695d0e09a675360b21c2
aapupu <94039288+aapupu@users.noreply.github.com>
2024-04-30 17:12:09 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -12,47 +12,30 @@ with open('requirements.txt') as f:
 HERE = pathlib.Path(__file__).parent
 README = (HERE / "README.md").read_text()
 
-import codecs
-import os.path
-
-def read(rel_path):
-    here = os.path.abspath(os.path.dirname(__file__))
-    with codecs.open(os.path.join(here, rel_path), 'r') as fp:
-        return fp.read()
-
-def get_version(rel_path):
-    for line in read(rel_path).splitlines():
-        if line.startswith('__version__'):
-            delim = '"' if '"' in line else "'"
-            return line.split(delim)[1]
-    else:
-        raise RuntimeError("Unable to find version string.")
-
-
-setup(name='mist',
-      version=get_version(HERE / "mist/__init__.py"),
-      packages=find_packages(),
-      description='',
-      long_description=README,
-
-      author='Lai Wenpu',
-      author_email='kyzy850520@163.com',
-      url='https://github.com/aapupu/MIST',
-      scripts=['MIST.py'],
-      install_requires=requirements,
-      python_requires='>3.7',
-      license='MIT',
-
-      classifiers=[
+setup(
+        name='mist',
+        version='1.0.0',
+        author='Lai Wenpu',
+        author_email='kyzy850520@163.com',
+        description='MIST: Deep Learning Integration of Single-Cell RNA and TCR Sequencing Data for T-Cell Insight',
+        long_description=README,
+        packages=find_packages(),
+        install_requires=requirements,
+        url='https://github.com/aapupu/MIST',
+        scripts=['MIST.py'],
+        python_requires='>=3.7.3',
+        license='GPL-3.0',
+        
+        classifiers=[
           'Development Status :: 3 - Alpha',
           'Intended Audience :: Science/Research',
-          'License :: OSI Approved :: MIT License',
+          'License :: OSI Approved :: GPL-3.0 License',
           'Programming Language :: Python :: 3.7.3',
-          # 'Operating System :: MacOS :: MacOS X',
-          # 'Operating System :: Microsoft :: Windows',
           'Operating System :: POSIX :: Linux',
+          'Operating System :: MacOS :: MacOS X',
+          'Operating System :: Microsoft :: Windows',
           'Topic :: Scientific/Engineering :: Bio-Informatics',
           'Framework :: PyTorch',
           'Environment :: Console',
-     ],
-     )
+        ]
+)
