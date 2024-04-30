8e4af0761cda807ef3a3629514164065152269e6
aapupu <94039288+aapupu@users.noreply.github.com>
2024-04-30 13:19:06 +0800

    Update MIST.py

--- MIST.py
+++ MIST.py
@@ -4,116 +4,83 @@ import argparse
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='MIST: Deep Learning Integration of Single-Cell RNA and TCR Sequencing Data for T-Cell Insight')
     
-    parser.add_argument('--data_list', '-d', type=str, nargs='+', default=[])
-    parser.add_argument('--batch_categories', '-b', type=str, nargs='+', default=None)
-    parser.add_argument('--join', type=str, default='inner')
-    parser.add_argument('--batch_key', type=str, default='batch')
-    parser.add_argument('--batch_name', type=str, default='batch')
-    parser.add_argument('--profile', type=str, default='RNA')
-    parser.add_argument('--test_list', '-t', type=str, nargs='+', default=[])
+    parser.add_argument('--rna_path', '-r', type=str, nargs='*', default=None)
+    parser.add_argument('--tcr_path', '-t', type=str, nargs='*', default=None)
+    parser.add_argument('--protein_path', '-p', type=str, nargs='?', default=None)
+    parser.add_argument('--batch', '-b', type=str, nargs='*', default=None)
+    parser.add_argument('--rna_data_type', '-rt', type=str, nargs='?', default='h5ad')
+    parser.add_argument('--tcr_data_type','-tt', type=str, nargs='?', default='10X')
+    parser.add_argument('--type', type=str, nargs=1, default='multi')
     
-    parser.add_argument('--min_features', type=int, default=None)
+    parser.add_argument('--min_genes', type=int, default=600)
     parser.add_argument('--min_cells', type=int, default=3)
-    parser.add_argument('--n_top_features', default=None)
-    parser.add_argument('--target_sum', type=int, default=None)
-    parser.add_argument('--processed', action='store_true', default=False)
-    parser.add_argument('--fraction', type=float, default=None)
-    parser.add_argument('--n_obs', type=int, default=None)
-    parser.add_argument('--use_layer', type=str, default='X')
+    parser.add_argument('--pct_mt', type=int, default=None)
+    parser.add_argument('--n_top_features', default=2000)
     parser.add_argument('--backed', action='store_true', default=False)
-
-    parser.add_argument('--projection', '-p', default=None)
-    parser.add_argument('--impute', type=str, default=None)
-    parser.add_argument('--outdir', '-o', type=str, default='output/')
+    parser.add_argument('--batch_scale', action='store_true', default=False)
+    parser.add_argument('--batch_min', type=int, default=0)
+    parser.add_argument('--remove_TCRGene', action='store_true', default=False)
     
-    parser.add_argument('--lr', type=float, default=2e-4)
-    parser.add_argument('--batch_size', type=int, default=64)
-    parser.add_argument('-g','--gpu', type=int, default=0)
-    parser.add_argument('--max_iteration', type=int, default=30000)
-    parser.add_argument('--seed', type=int, default=124)
-    parser.add_argument('--chunk_size', type=int, default=20000)
-    parser.add_argument('--ignore_umap', action='store_true')
-    parser.add_argument('--repeat', action='store_true')
-    parser.add_argument('--assess', action='store_true')
-    parser.add_argument('--eval', action='store_true')
-    parser.add_argument('--num_workers', type=int, default=4)
-    parser.add_argument('--keep_mt', action='store_true')
-    # parser.add_argument('--version', type=int, default=2)
-    # parser.add_argument('--k', type=str, default=30)
-    # parser.add_argument('--embed', type=str, default='UMAP')
-#     parser.add_argument('--beta', type=float, default=0.5)
-#     parser.add_argument('--hid_dim', type=int, default=1024)
+    # parser.add_argument('--max_len', type=int, default=30)
+    # parser.add_argument('--scirpy', action='store_true', default=True)
+    
+    parser.add_argument('--batch_size', type=int, default=128)
+    parser.add_argument('--num_workers', type=int, default=8)
+    parser.add_argument('--pooling_dims', type=int, default=16)
+    parser.add_argument('--z_dims', type=int, default=128)
+    parser.add_argument('--drop_prob', type=float, default=0.1)
+    parser.add_argument('--lr', type=float, default=1e-4)
+    parser.add_argument('--weight_decay', type=float, default=1e-3)
+    parser.add_argument('--max_epoch', type=int, default=400)
+    parser.add_argument('--patience', type=int, default=40)
+    parser.add_argument('--warmup', type=int, default=40)
+    parser.add_argument('--penalty', type=str, default='mmd_rbf')
+    
+    parser.add_argument('--gpu', type=int, default=0)
+    parser.add_argument('--seed', type=int, default=42)
+    parser.add_argument('--outdir', '-o', type=str, default='mist_output/')
 
-
+    # parser.add_argument('--aa_dims', type=int, default=64)
+    # parser.add_argument('--gene_dims', type=int, default=48)
+    # parser.add_argument('--weights', action='store_true', default=False)
+    
     args = parser.parse_args()
     
     from mist import MIST
     adata, model = MIST(
-    rna_path:List[str]=None, 
-    tcr_path:List[str]=None, 
-    batch:List[str]=None,
-    rna_data_type:str='h5ad',
-    tcr_data_type:str='10X',
-    protein_path:str=None,
-    type:str='multi',         
-    min_genes:int=600, 
-    min_cells:int=3, 
-    pct_mt:int=None,
-    n_top_genes:int=2000, 
-    backed:bool=False,
-    batch_scale:bool=False,
-    batch_min:int=0,
-    remove_TCRGene:bool=False,
-    max_len:int=30,
-    scirpy:bool=True,
-    batch_size:int=128,
-    num_workers:int=8,
-    aa_dims:int=64,
-    gene_dims:int=48,
-    pooling_dims:int=16,
-    z_dims:int=128,
-    drop_prob:float=0.1,
-    weights:bool=False,
-    lr:float=1e-4,
-    weight_decay:float=1e-3,
-    max_epoch:int=400, 
-    patience:int=40, 
-    warmup:int=40,
-    penalty:str='mmd_rbf',
-    gpu:int=0, 
-    seed:int=42,
-    outdir:str=None
-        args.data_list, 
-        batch_categories=args.batch_categories,
-        profile=args.profile,
-        join=args.join, 
-        batch_key=args.batch_key, 
-        min_features=args.min_features, 
+        rna_path=args.rna_path, 
+        tcr_path=args.tcr_path, 
+        batch=args.batch,
+        rna_data_type=args.rna_data_type,
+        tcr_data_type=args.tcr_data_type,
+        protein_path=args.protein_path,
+        type=args.type,         
+        min_genes=args.min_genes, 
         min_cells=args.min_cells, 
-        target_sum=args.target_sum,
-        n_top_features=args.n_top_features, 
-        fraction=args.fraction,
-        n_obs=args.n_obs,
-        processed=args.processed,
-        keep_mt=args.keep_mt,
-        use_layer=args.use_layer,
+        pct_mt=args.pct_mt,
+        n_top_genes=args.n_top_genes, 
         backed=args.backed,
-        batch_size=args.batch_size, 
-        lr=args.lr, 
-        max_iteration=args.max_iteration, 
-        impute=args.impute,
-        batch_name=args.batch_name, 
-        seed=args.seed, 
-        gpu=args.gpu, 
-        outdir=args.outdir, 
-        projection=args.projection, 
-        chunk_size=args.chunk_size,
-        ignore_umap=args.ignore_umap,
-        repeat=args.repeat,
-        verbose=True,
-        assess=args.assess,
-        eval=args.eval,
+        batch_scale=args.batch_scale,
+        batch_min=args.batch_min,
+        remove_TCRGene=args.remove_TCRGene,
+        max_len=30,
+        scirpy=True,
+        batch_size=args.batch_size,
         num_workers=args.num_workers,
-        show=False,
+        aa_dims=64,
+        gene_dims=48,
+        pooling_dims=args.pooling_dims,
+        z_dims=args.z_dims,
+        drop_prob=args.drop_prob,
+        weights=False,
+        lr=args.lr,
+        weight_decay=args.weight_decay,
+        max_epoch=args.max_epoch,
+        patience=args.patience, 
+        warmup=args.warmup,
+        penalty=args.penalty,
+        gpu=args.gpu, 
+        seed=args.seed,
+        outdir=args.outdir
     )
         
