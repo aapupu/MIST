dae980f911796f84f865826a4f7b6feae6f5e3a7
aapupu <94039288+aapupu@users.noreply.github.com>
2024-04-30 16:59:33 +0800

    Update __init__.py

--- mist/__init__.py
+++ mist/__init__.py
@@ -1,7 +1,3 @@
 #!/usr/bin/env python
-__version__ = '1.0.0'
-__author__ = 'Lai Wenpu'
-__email__ = 'kyzy850520@163.com'
-
 from .founction import MIST, remove_redundant_genes, label_tcr_similarity, label_tcr_dist, label_transfer
 from .magic_modify import magic_modify
