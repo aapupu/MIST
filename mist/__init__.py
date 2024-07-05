3d2d6c69574aa72f1f10831854f51fc079fea9bc
aapupu <kyzy850520@163.com>
2024-07-06 02:45:35 +0800

    Your commit message

--- mist/__init__.py
+++ mist/__init__.py
@@ -1,3 +1,3 @@
 #!/usr/bin/env python
-from .founction import MIST, remove_redundant_genes, label_tcr_similarity, label_tcr_dist, label_transfer
+from .function import MIST, remove_redundant_genes, label_tcr_similarity, label_tcr_dist, label_transfer
 from .magic_modify import magic_modify
